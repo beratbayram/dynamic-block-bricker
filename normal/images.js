2e88608abb00f016f50360680273d642a71935dc
Berat BAYRAM <beratbayram7@gmail.com>
2021-03-19 14:03:48 +0200

    Added resource links

--- normal/images.js
+++ normal/images.js
@@ -1,24 +1,37 @@
+// 1st Level Background: 
+// https://image.winudf.com/v2/image1/Y29tLndNRFJ1bm5lcl84ODk0NDI2X3NjcmVlbl8wXzE1NTYwNDEyMzhfMDE2/screen-0.jpg?fakeurl=1&type=.jpg 
 const BG1_IMG = new Image();
 BG1_IMG.src = "img/bg1.png";
 
+// 2nd Level Background:
+// https://cutewallpaper.org/21/unity-scrolling-background/3-Parallax-Backgrounds-OpenGameArtorg.png
 const BG2_IMG = new Image();
 BG2_IMG.src = "img/bg2.png";
 
+// 3rd Level Background:
+// https://cdnb.artstation.com/p/assets/images/images/011/627/973/medium/tiina-majava-winter1.jpg?1530561201
 const BG3_IMG = new Image();
 BG3_IMG.src = "img/bg3.jpg";
 
+// 4th Level Background:
+// https://calliedoart.files.wordpress.com/2015/02/cropped-paralax-background-3.jpg
 const BG4_IMG = new Image();
 BG4_IMG.src = "img/bg4.jpg";
 
+// 5th Level Background:
+// https://i.redd.it/9udbh5cmidm31.png
 const BG5_IMG = new Image();
 BG5_IMG.src = "img/bg5.png";
 
+// http://pixsector.com/icon/black-heart-icon-png-vector/613
 const LIFE_IMG = new Image();
 LIFE_IMG.src = "img/life.png";
 
+// https://png.pngtree.com/png-vector/20190416/ourmid/pngtree-red-heart-vector-png-image_949206.jpg
 const FULL_LIFE_IMG = new Image();
 FULL_LIFE_IMG.src = "img/fullLife.png";
 
+// https://participa.utalca.cl/web/wp-content/themes/utalca/img/icons/iconos-capitulos/cap-3-b.svg
 const LEVEL_IMG = new Image();
 LEVEL_IMG.src = "img/level.png";
 
